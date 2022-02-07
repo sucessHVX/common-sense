7fd7d0ada59f6d61aa94c496f4475505ed1cde86
Jaemin Ha <woals3527@naver.com>
2022-02-07 18:09:56 +0900

    we

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const onClick = () => {
+    alert('개꿀');
+  };
+
   return (
     <div className="App">
       <h1>상식 퀴즈</h1>
       <img className="firstImage" src="img/1.jpg"></img>
-      <button>시작</button>
+      <button onClick={onClick} className="startButton">
+        시작
+      </button>
     </div>
   );
 }
