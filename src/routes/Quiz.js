8b856fe83102a33fcf08aa9668d325e080c1cab4
Jaemin Ha <woals3527@naver.com>
2022-02-24 18:03:22 +0900

    zz

--- src/routes/Quiz.js
+++ src/routes/Quiz.js
@@ -1,9 +1,10 @@
+import styles from './Quiz.module.css';
+
 export default function Quiz() {
   return (
     <>
       <h1>문제</h1>
-      <h2>흠 어떻게 할까?</h2>
-      <h3>컴포넌트를 재사용하려면 어떻게 하는게 좋지?</h3>
+      <canvas className={styles.canvas}></canvas>
     </>
   );
 }
